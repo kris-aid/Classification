0dcc67273ecec678468b8f2e179fc36fc875fda9
sebas-rom <gabrielsebastian.romero@gmail.com>
2024-03-19 11:31:43 -0500

    graph best models

--- classification_decision_trees.py
+++ classification_decision_trees.py
@@ -5,10 +5,13 @@ from sklearn.tree import plot_tree
 import numpy as np
 from sklearn.model_selection import train_test_split
 import os
+from plots import graph_tree, generate_graphs
 
 folder_path = 'subsets_variable_length'
 dataframes=[]
+filenames =[]
 for filename in os.listdir(folder_path):
+    filenames.append(filename)
     file_path = os.path.join(folder_path, filename)
     if os.path.isfile(file_path):  # Check if it's a file (not a directory)
         print(filename)
@@ -16,26 +19,46 @@ for filename in os.listdir(folder_path):
     dataframes.append(df)
 # Create a DataFrame from the CSV file
 
-for df in dataframes:
 
+
+for df in dataframes:
+    filename = filenames.pop(0)
+    print("\n\n")
+    print(filename)
     X = df.drop(columns=['Etiqueta'])  # Features (all columns except the target)
     y = df['Etiqueta']
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
     y_mapped = y.map({0: "long events", 1: "short events"})
 
-
     models=generate_models(X_train,y_train)
 
+    criteria_list = ['entropy', 'gini','gain ratio']
     for criterion in models:
         print('///////')
+        citeria = criteria_list.pop(0)
+        print("  "+citeria+":")
+        best_model = None
+        best_auc = -1  
+        best_model_conf_matrix = None
+        best_model_k = None
+        i = 1
         for model in criterion:
+            i=i+1
             clf = model
             conf_matrix, accuracy, precision, recall, f1, auc, mcc=calculate_metrics(X_test,y_test,clf)
             print(accuracy, precision, recall, f1, auc, mcc)
+            # Check if the current model has a higher AUC
+            if auc > best_auc:
+                best_auc = auc
+                best_model = clf
+                best_model_conf_matrix = conf_matrix
+                best_model_k = i
+        print("\nConfusion Matrix - Best Model "+str(best_model_k)+" AUC: "+str(best_auc)) 
+        print(best_model_conf_matrix)
+        graph_tree(best_model, X.columns, y_mapped, citeria, str(best_model_k), show=False, dataset_name=filename)
 
-'''
-# Plot the Decision Tree
-plt.figure(figsize=(12, 8))
-plot_tree(clf, filled=True, feature_names=X.columns, class_names=y_mapped)
-plt.show()
-'''
+        best_model.fit(X_train, y_train)
+        y_pred = best_model.predict(X_test)
+        y_names= ["0", "1"]
+        generate_graphs(y_test, y_pred, citeria, show=False, dataset_name=filename)
+    
--- plots.py
+++ plots.py
@@ -1,3 +1,4 @@
+import os
 import pandas as pd
 from sklearn.tree import DecisionTreeClassifier, plot_tree
 import numpy as np
@@ -5,16 +6,16 @@ from sklearn.model_selection import StratifiedKFold, train_test_split
 from sklearn.metrics import auc, confusion_matrix, accuracy_score, precision_recall_curve, precision_score, recall_score, roc_auc_score, f1_score, matthews_corrcoef,roc_curve
 from matplotlib import pyplot as plt
 
-def generate_graphs(y_test, y_pred, criterion):
+def generate_graphs(y_test, y_pred, criterion, show = False, dataset_name = ''):
     fpr, tpr, thresholds = roc_curve(y_test, y_pred)
     roc_auc = auc(fpr, tpr)
-    graph_roc_curve(fpr, tpr,roc_auc,criterion= criterion)
+    graph_roc_curve(fpr, tpr,roc_auc,criterion= criterion, show = show  , dataset_name = dataset_name )
     
     precision, recall, _ = precision_recall_curve(y_test, y_pred)
-    graph_precision_recall_curve(precision, recall,criterion)
+    graph_precision_recall_curve(precision, recall,criterion, show = show  , dataset_name = dataset_name )
     
 
-def graph_roc_curve(fpr, tpr,roc_auc, criterion):
+def graph_roc_curve(fpr, tpr,roc_auc, criterion, show = False, dataset_name = ''):
     plt.plot(fpr, tpr, color='orange', label='ROC curve (AUC = %0.2f)' % roc_auc)
     plt.plot([0, 1], [0, 1], color='darkblue', linestyle='--')
     plt.xlim([0.0, 1.0])
@@ -23,50 +24,62 @@ def graph_roc_curve(fpr, tpr,roc_auc, criterion):
     plt.ylabel('True Positive Rate')
     plt.title('Receiver Operating Characteristic (ROC) Curve for criterion: '+criterion)
     plt.legend(loc="lower right")
-    plt.show()
+    plt.savefig(f'trees_output/{dataset_name}/{criterion}_ROC_curve.png')
+    if show:
+        plt.show()
+    plt.close()
+    
 
-def graph_precision_recall_curve(precision, recall,criterion):
+def graph_precision_recall_curve(precision, recall,criterion, show = False, dataset_name = ''):
     plt.plot(recall, precision, marker='.', label='Precision-Recall curve', color = 'orange')
     plt.xlabel('Recall')
     plt.ylabel('Precision')
     plt.title('Precision-Recall Curve for criterion: '+criterion)
     plt.legend(loc="lower right")
-    plt.show()
+    plt.savefig(f'trees_output/{dataset_name}/{criterion}_Precision-Recall.png')
+    if show:
+        plt.show()
+    plt.close()
     
-def graph_tree(classifier, x_names,y_names, criterion,subset_name):
-    plt.figure(figsize=(40, 30),dpi=200)
+def graph_tree(classifier, x_names,y_names, criterion,subset_name, show = False, dataset_name = ''):
+    os.makedirs(f"trees_output/{dataset_name}", exist_ok=True)
+    fig = plt.figure(figsize=(40, 30), dpi=200)
     _ = plot_tree(classifier, 
                    feature_names=x_names,  
                    class_names=y_names,
                    filled=True)
     #save the tree as a png image with the name of the dataset and the classifier
-    plt.savefig('trees_output/tree criterion : '+criterion+' '+ subset_name +'.png')
-    plt.show()
+    plt.savefig(f'trees_output/{dataset_name}/{criterion}_k-{subset_name}_tree.png')
+    if show:
+        plt.show()
+    plt.close(fig)
+
     
     
-dataset = 'subsets_variable_length/subset_1_34_df.csv'
-data = pd.read_csv(dataset)
-X = data.drop('Etiqueta', axis=1).values
-x_names = data.drop('Etiqueta', axis=1).columns
-y = data['Etiqueta'].values
-y_names = data['Etiqueta'].unique()
-criterion ='gini'
-classifier=DecisionTreeClassifier(criterion=criterion, max_depth=3, random_state=42)
+def test():
+    dataset = 'subsets_variable_length/subset_1_34_df.csv'
+    data = pd.read_csv(dataset)
+    X = data.drop('Etiqueta', axis=1).values
+    x_names = data.drop('Etiqueta', axis=1).columns
+    y = data['Etiqueta'].values
+    y_names = data['Etiqueta'].unique()
+    criterion ='gini'
+    classifier=DecisionTreeClassifier(criterion=criterion, max_depth=3, random_state=42)
 
-skf = StratifiedKFold(n_splits=10, shuffle=True, random_state=42)
-for train_index, test_index in skf.split(X, y):
-    X_train, X_test = X[train_index], X[test_index]
-    y_train, y_test = y[train_index], y[test_index]
+    skf = StratifiedKFold(n_splits=10, shuffle=True, random_state=42)
+    for train_index, test_index in skf.split(X, y):
+        X_train, X_test = X[train_index], X[test_index]
+        y_train, y_test = y[train_index], y[test_index]
 
-    classifier.fit(X_train, y_train)
-    y_pred = classifier.predict(X_test)
-    y_names= ["0", "1"]
-    #generate_graphs(y_test, y_pred, criterion)
-    graph_tree(classifier, x_names,y_names, criterion,"subset_1_34_df")
+        classifier.fit(X_train, y_train)
+        y_pred = classifier.predict(X_test)
+        y_names= ["0", "1"]
+        generate_graphs(y_test, y_pred, criterion)
+        # graph_tree(classifier, x_names,y_names, criterion,"subset_1_34_df")
     
 #Choose the best model based on the AUC metric
 
-
+# test()
 # best_model = max(results, key=results.get)
 # print(best_model)
 # print(results[best_model])
